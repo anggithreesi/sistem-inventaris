d30999d1acf48ed95bf222cb671d23d3f1b92a19
Anggitri Sihombing <anggitrisihombing@gmail.com>
2021-05-24 13:15:41 +0700

    menambahkan aksi aktivasi-nonaktivasi

--- application/models/Main_model.php
+++ application/models/Main_model.php
@@ -1,12 +1,13 @@
 <?php
 
-class Main_model extends CI_Model{
+class Main_model extends CI_Model
+{
 
     public function select_record($table, $where = '')
     {
 
         $this->db->select()->from($table);
-        if ($where){
+        if ($where) {
             $this->db->where($where);
         }
         $query = $this->db->get();
@@ -16,12 +17,11 @@ class Main_model extends CI_Model{
     public function save_record($table, $data)
     {
         $this->db->insert($table, $data);
-        if($query = $this->db->affected_rows() > 0){
-            return TRUE; 
+        if ($query = $this->db->affected_rows() > 0) {
+            return TRUE;
         } else {
             return FALSE;
         }
-
     }
 
     public function delete_record($table, $field_name, $id)
@@ -34,16 +34,18 @@ class Main_model extends CI_Model{
             return false;
     }
 
-    public function single_record($table, $where = ''){
+    public function single_record($table, $where = '')
+    {
         $this->db->select()->from($table);
-        if ($where){
+        if ($where) {
             $this->db->where($where);
         }
         $query = $this->db->get();
         return $query->row();
     }
 
-    public function update_record($table, $data, $where){
+    public function update_record($table, $data, $where)
+    {
         $this->db->where($where);
         $query = $this->db->update($table, $data);
         if ($this->db->affected_rows() > 0)
@@ -68,5 +70,14 @@ class Main_model extends CI_Model{
 
         return $result = $query->result();
     }
- 
+
+    public function ubahaktifpegawai($id, $status)
+    {
+        $atur = array(
+            'STATUS' => $status
+        );
+
+        $this->db->where('EMP_ID', $id);
+        $this->db->update('pegawai', $atur);
+    }
 }
